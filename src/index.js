6346f110087d7a2e5598c816fd01ddf087f21680
Sadvi-Konduri <kondurisadvi@gmail.com>
2024-07-09 09:10:35 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ root.render(
    <Router>
     <Navbar/>
     <p>HELLO WELCOME</p>
+    <P>THIS IS TRAINING</P>
     <Routes>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
